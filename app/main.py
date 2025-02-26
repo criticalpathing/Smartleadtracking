715d7e94f2642a559a754d06e5dc7b91cea17a17
JacobGalajda <jegalajda13@knights.ucf.edu>
2025-02-26 16:16:53 -0500

    Update

--- app/main.py
+++ app/main.py
@@ -4,15 +4,19 @@ from hubspot import HubSpot
 
 
 
-HUBSPOT_API_KEY = os.getenv("HUBSPOT_API_KEY")
 
-if HUBSPOT_API_KEY is None or HUBSPOT_API_KEY == "":
-    raise Exception("HUBSPOT API KEY is empty.")
+HUBSPOT_API_KEY = os.getenv("HUBSPOT_API_KEY")
+APOLLO_API_KEY = os.getenv("APOLLO_API_KEY")
+
+if (HUBSPOT_API_KEY is None and HUBSPOT_API_KEY == "") or (APOLLO_API_KEY is None and APOLLO_API_KEY == ""):
+    raise Exception("Missing API Key(s) in environment variables.")
 
 api_client = HubSpot(access_token=HUBSPOT_API_KEY)
-
 api_client.crm.companies.basic_api.update_with_http_info()
 
+
+
+# TODO(ALLAN): Implement
 def get_company(company_url : str, company_name : str = None):
     '''
     Takes a company_url, returns the Company Object from Hubspot. If one is not found, a new company is created.
@@ -29,22 +33,33 @@ def get_company(company_url : str, company_name : str = None):
 
     pass
 
-def lead_enrich(company_object : object):
-    # Pass whole Company Object from HubSpot into Lead Enrichment Module
+def lead_enrich(company_url : str):
+
+    # Check if the company_url is a valid domain
+    # example.com is valid
+    # www.example.com is not
+    # allan@example.com is not
+
+    url = str("https://api.apollo.io/api/v1/organizations/enrich?domain=" + company_url)
+
     headers = {
-
+        "accept": "application/json",
+        "Cache-Control": "no-cache",
+        "Content-Type": "application/json",
+        'x-api-key': APOLLO_API_KEY
     }
-    data = {
 
-    }
+    response = requests.get(url, headers=headers)
 
-    # requests.post("some URL of a provider that does lead enrichment", headers=headers, payload=data)
+    if response.status_code != 200:
+        raise Exception("Whoa there cowboy! What happened here? " + str(response.text))
+    
+    res = response.json()
+    return res
 
-    pass
-
+# TODO(ALLAN): Implement
 def add_enriched_data_to_hubspot(data: dict):
     pass
-
 
 def lambda_handler(event, context):
 
@@ -63,27 +78,7 @@ def lambda_handler(event, context):
 
     company = get_company(company_url, company_name=company_name)
 
-    lead_enrich(company)
-
-    '''
-    Return the following structure
-
-    Contacts: [
-        {
-            firstName,
-            lastName,
-            email,
-            phone,
-            title,
-            data: [
-                # additional fields that may or may not exist depending on lead enrichment source
-            ]
-        }
-    ]
-    # Company: {
-    #     ...
-    # }
-    '''
+    enriched_data = lead_enrich(company)
 
     # Attach contacts from JSON structure to company
     # -- Search for contact. If exists, use this one and update any info. Else, create contact and use this one
